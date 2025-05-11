bb59eef2cf70756a61988138adecff71f63c0d5c
Aya Hadidi <aya.hadidi03@gmail.com>
2025-05-11 15:53:26 +0300

    adding authincticationSerializer

---
+++ Backend/serializers/AuthenticationSerializer.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from ..models import User
+
+class AuthenticationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'email', 'password', 'phone_number', 'location']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
