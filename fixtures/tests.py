31d71a7f516755de72a1444c1b6b712a7b733f0b
Yasmin <yasmeenalmohtaseb@gmail.com>
2025-05-14 12:51:43 +0300

    Fetch data from tables

--- fixtures/tests.py
+++ fixtures/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from Backend.models import Colors
 from Backend.models import PromoCode
+from Backend.models import UserPromoCode
 class ColorTestCase(TestCase):
     fixtures=['color.json']
     def test_color_count(self):
@@ -10,3 +11,9 @@ class PromoCodeTestCase(TestCase):
     fixtures=['promoCode.json']
     def test_promoCode_count(self):
         self.assertEqual(PromoCode.objects.count(),20)
+
+class UserPromoCodeTestCase(TestCase):
+    fixtures=['user_promoCode_fixture.json']
+    def test_promoCode_count(self):
+        self.assertEqual(UserPromoCode.objects.count(),20)
+
