03fd6c469fa3ce52458512f31078e0a58ca32d4f
Yasmin <yasmeenalmohtaseb@gmail.com>
2025-06-16 17:20:32 +0300

    Images solved

--- glowe/urls.py
+++ glowe/urls.py
@@ -26,6 +26,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -49,4 +51,4 @@ urlpatterns = [
     path('userProfile/',UserProfile_view.as_view()),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
