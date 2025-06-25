665ebf2822dbddd0bccc2b6661a3320129405e23
NEalnajjar7 <noureddeenalnajjar@gmail.com>
2025-06-25 16:46:01 +0300

    add product comments endpoint

---
+++ Backend/views/product_comments_view.py
@@ -0,0 +1,16 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from django.db.models import Q
+from Backend.models.rating_model import Rating
+from Backend.serializers.rating_serializer import ProductCommentsSerializer
+
+class AllCommentsView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, product_id):
+        ratings = Rating.objects.filter(product_id=product_id).exclude(
+            Q(comment__isnull=True) | Q(comment__regex=r'^\s*$')
+        )
+        serializer = ProductCommentsSerializer(ratings, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
