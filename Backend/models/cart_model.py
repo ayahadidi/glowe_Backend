477cf13fa8cd61db4617c49be0e63461326acdfd
Yasmin <yasmeenalmohtaseb@gmail.com>
2025-06-29 14:07:04 +0300

    edit cart status

--- Backend/models/cart_model.py
+++ Backend/models/cart_model.py
@@ -4,10 +4,8 @@ import uuid
 from django.core.validators import MaxValueValidator
 
 class CartStatus(models.IntegerChoices):
-    PENDING=1,'pending'
-    ACTIVE=2,'active'
-    EXPIRED=3,'expired'
-    SAVED_FOR_LATER=4,'saved_for_later'
+    ACTIVE=1,'active'
+    CHECKED_OUT=2,'checked_out'
 
 
 
@@ -15,6 +13,6 @@ class Cart(models.Model):
     id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     total_price=models.FloatField(validators=[MaxValueValidator(100000)],default=0)
     total_items=models.IntegerField(validators=[MaxValueValidator(100)],default=0)
-    type=models.IntegerField(validators=[MaxValueValidator(10)],default=2)
+    type=models.IntegerField(validators=[MaxValueValidator(10)],default=1)
     promocode=models.ForeignKey('Backend.PromoCode',on_delete=models.CASCADE,default=1)
     user=models.ForeignKey('custom_user.User',on_delete=models.CASCADE)
